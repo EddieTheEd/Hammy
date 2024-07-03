b74fe66312c19d979a7901969179f7a5cd9cecb9
edwardhuynh <edwardleehuynh@gmail.com>
2024-07-03 15:27:10 +0800

    Prototype input window
    - Testing a new input window which saves its input and passes it to main. Will ahve to consider error-handling for this input, depending on what is needed.

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,9 @@ module Main where
 import Data.GI.Base
 import qualified GI.Gtk as Gtk
 import Control.Monad (void)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.IORef
 
 import Todo
 import WinShortcut
@@ -13,6 +16,10 @@ import WinShortcut
 main :: IO ()
 main = do
   Gtk.init Nothing
+
+  userInput <- createInputWindow 
+
+  putStrLn $ "User input: " ++ userInput
 
   win <- new Gtk.Window [ #title := "Hammy"
                         , #defaultWidth := 750
@@ -27,8 +34,8 @@ main = do
   (todoBox, entry, listBox) <- createTodoUI
   tabLabel1 <- new Gtk.Label [#label := "Todo List"]
   
-  #appendPage notebook todoBox (Just tabLabel1)
-  #add win notebook
+  Gtk.notebookAppendPage notebook todoBox (Just tabLabel1)
+  Gtk.containerAdd win notebook
   
   handleWinShortcuts win
   
@@ -37,4 +44,37 @@ main = do
   
   on win #destroy Gtk.mainQuit
   #showAll win
+
   Gtk.main
+
+createInputWindow :: IO String
+createInputWindow = do
+  win <- new Gtk.Window [ #title := "Input Window"
+                        , #defaultWidth := 300
+                        , #defaultHeight := 150
+                        , #windowPosition := Gtk.WindowPositionCenter
+                        , #decorated := True
+                        , #resizable := True
+                        ]
+
+  entry <- new Gtk.Entry [#placeholderText := "Enter your input here"]
+
+  button <- new Gtk.Button [#label := "Submit"]
+
+  box <- new Gtk.Box [#orientation := Gtk.OrientationVertical, #spacing := 10]
+  Gtk.containerAdd box entry
+  Gtk.containerAdd box button
+  Gtk.containerAdd win box
+
+  userInput <- newIORef ""
+
+  void $ on button #clicked $ do
+    input <- Gtk.entryGetText entry
+    writeIORef userInput (T.unpack input)
+    Gtk.widgetDestroy win 
+    Gtk.mainQuit
+
+  #showAll win
+  Gtk.main
+  readIORef userInput
+  -- can now be used by main!
